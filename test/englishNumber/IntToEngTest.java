e2cea2d8d918c0e3b1952bd6f3bc505ab85b3ef7
GingaSystem <37409683+GingaSystem@users.noreply.github.com>
2020-06-12 18:28:28 +0900

    境界値テストを追加しました

--- test/englishNumber/IntToEngTest.java
+++ test/englishNumber/IntToEngTest.java
@@ -13,5 +13,40 @@ public class IntToEngTest {
 		String actual = itg.printEnglishNumber(62);
 		assertThat(actual, is(expected));
 	}
+	@Test
+	public void キーボードで0と入力した時の出力結果() {
+		IntToEng itg = new IntToEng();
+		String expected = "zero";
+		String actual = itg.printEnglishNumber(0);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void キーボードで100と入力した時の出力結果() {
+		IntToEng itg = new IntToEng();
+		String expected = "one hundred";
+		String actual = itg.printEnglishNumber(100);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void キーボードで19と入力した時の出力結果() {
+		IntToEng itg = new IntToEng();
+		String expected = "nineteen";
+		String actual = itg.printEnglishNumber(19);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void キーボードで20と入力した時の出力結果() {
+		IntToEng itg = new IntToEng();
+		String expected = "twenty";
+		String actual = itg.printEnglishNumber(20);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void キーボードで21と入力した時の出力結果() {
+		IntToEng itg = new IntToEng();
+		String expected = "twenty one";
+		String actual = itg.printEnglishNumber(21);
+		assertThat(actual, is(expected));
+	}
 
 }
